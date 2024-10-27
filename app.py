2d5da84debd5abcf22907c2450576ccf1a51c299
Satyajeet1396 <36727765+Satyajeet1396@users.noreply.github.com>
2024-10-27 13:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -86,5 +86,7 @@ if user_file:
     st.success("Note: In the downloaded Excel file, journal names, best matches, and impact factors highlighted in yellow indicate entries that were matched using fuzzy matching. This means the journal name in your file didn’t exactly match any name in the database but was closely matched based on similarity. Direct matches without any modification are left unhighlighted.")
 else:
     st.info("Please upload your Journal Names Excel file to proceed.")
+
+
 st.info("Created by Dr. Satyajeet Patil")
 st.info("For more cool apps like this visit: https://patilsatyajeet.wixsite.com/home/python")
